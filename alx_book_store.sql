208157df525034685bafc04c9ea6721b49fdd0d7
james-mungai-git <j.murigi.mungai@gmail.com>
2025-10-20 08:41:09 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -41,6 +41,8 @@ CREATE TABLE Order_details (
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    Order_Details
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+
 ) ENGINE=INNODB;
